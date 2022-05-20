3f30d1c1fe1e2c1ee59446f830c0f8dee8ef7428
Ahmed_Kandil <Ahmed_Kandil@DESKTOP-UREEC09>
2022-05-20 16:31:05 +0200

    Add project files.

---
+++ Read_Ports/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.IO.Ports;
+
+namespace Read_Ports
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void button1_Click(object sender, EventArgs e)
+            {
+
+            System.Diagnostics.Process.Start( "https://web.facebook.com/ahmed.kandil.777701/");
+
+
+        }
+        
+    }
+}
